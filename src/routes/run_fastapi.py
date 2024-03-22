c56a8f635541bd42159c59b3b82105fa6e88d453
airtoour <artur.gips@mail.ru>
2024-03-23 00:44:06 +0700

    дебаг идет полным ходом

--- src/routes/run_fastapi.py
+++ src/routes/run_fastapi.py
@@ -1,10 +1,23 @@
 if __name__ == '__main__':
     import uvicorn
+    import os
+
     from src.db.config import app
     from get_env import get_env
 
-    uvicorn.run(app,
-                host=get_env("FASTAPI_HOST"),
-                port=int(get_env("FASTAPI_PORT")),
-                ssl_keyfile=None,
-                ssl_certfile=None)
+    path_to_cert = "C:/cert"
+
+    ssl_keyfile_name = 'privateKey.key'
+    ssl_certfile_name = 'certificate.crt'
+
+    ssl_keyfile = os.path.join(path_to_cert, ssl_keyfile_name)
+    ssl_certfile = os.path.join(path_to_cert, ssl_certfile_name)
+
+    if ssl_keyfile and ssl_certfile:
+        uvicorn.run(app,
+                    host=get_env("FASTAPI_HOST"),
+                    port=int(get_env("FASTAPI_PORT")),
+                    ssl_keyfile=ssl_keyfile,
+                    ssl_certfile=ssl_certfile)
+    else:
+        print('NOT')
--- src/telegram/keyboards/inline/inline.py
+++ src/telegram/keyboards/inline/inline.py
@@ -5,7 +5,7 @@ from get_env import get_env
 def signup_tap_link(user_id: int) -> InlineKeyboardMarkup:
     from src.db.config import app
 
-    url = f'https://{get_env("FASTAPI_HOST")}:{get_env("FASTAPI_PORT")}/{app.url_path_for("signup", user_id=user_id)}'
+    url = f'https://{get_env("FASTAPI_HOST")}:{get_env("FASTAPI_PORT")}{app.url_path_for("signup", user_id=user_id)}'
     markup = InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text='Регистрация', web_app=WebAppInfo(url=url))]
     ])
